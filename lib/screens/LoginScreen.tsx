fcc1b2cbc1064b2547fffcb611f9e8f9a1812001
qd <chuaqindi123@gmail.com>
2025-04-06 15:42:43 +0800

    add ui style

--- lib/screens/LoginScreen.tsx
+++ lib/screens/LoginScreen.tsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react'
-import { View, Text, TextInput, Button, StyleSheet } from 'react-native'
+import { View, Text, TextInput, Button, StyleSheet , ImageBackground, TouchableOpacity} from 'react-native'
 import { supabase } from '../supabase'
+import { SafeAreaView } from 'react-native-safe-area-context'
 
 export default function LoginScreen({ navigation }: any) {
   const [email, setEmail] = useState('')
@@ -19,12 +20,16 @@ export default function LoginScreen({ navigation }: any) {
   }
 
   return (
-    <View style={styles.container}>
+    <ImageBackground source={{ uri: 'https://images.unsplash.com/photo-1540206395-68808572332f?q=80&w=2126&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D' }}
+    style={styles.background} resizeMode="cover">
+      <SafeAreaView style={styles.container}>
+
       <Text style={styles.title}>Login</Text>
 
       <TextInput
         style={styles.input}
         placeholder="Email"
+        placeholderTextColor="black"
         value={email}
         onChangeText={setEmail}
         autoCapitalize="none"
@@ -33,20 +38,45 @@ export default function LoginScreen({ navigation }: any) {
       <TextInput
         style={styles.input}
         placeholder="Password"
+        placeholderTextColor="black"
         value={password}
         onChangeText={setPassword}
         secureTextEntry
       />
 
-      <Button title="Login" onPress={handleLogin} />
+      <TouchableOpacity style={styles.button} onPress={handleLogin}>
+        <Text style={styles.buttonText}>Login</Text>
+      </TouchableOpacity>
+
       <Text style={styles.message}>{message}</Text>
-    </View>
+    </SafeAreaView>
+    </ImageBackground>
   )
 }
 
 const styles = StyleSheet.create({
-  container: { flex: 1, justifyContent: 'center', padding: 20 },
+  background: { flex: 1, justifyContent: 'center', padding: 20 },
+  container: { flex: 0.7, justifyContent: 'center', padding: 20, backgroundColor: 'rgba(255, 255, 255, 0.8)', borderRadius: 20,},
+  scrollview: { flex: 1},
+  
   title: { fontSize: 24, marginBottom: 20, textAlign: 'center' },
   input: { borderWidth: 1, marginBottom: 10, padding: 10, borderRadius: 5 },
-  message: { marginTop: 20, textAlign: 'center' },
+  message: { marginTop: 20, textAlign: 'center', color: 'red' },
+
+  button: {
+    backgroundColor: '#4CAF50',
+    paddingVertical: 12,
+    paddingHorizontal: 24,
+    borderRadius: 8,
+    alignItems: 'center',
+    marginTop: 16,
+    elevation: 3, // adds a slight shadow for Android
+  },
+ 
+  buttonText: {
+    color: '#fff',
+    fontSize: 16,
+    fontWeight: 'bold',
+  },
+
 })
